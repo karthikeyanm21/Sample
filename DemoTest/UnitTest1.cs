8ee880f5035dc2e2f6732c929d7b4c3803ff6c0d
Karthikeyan M <karthikeyan@cindura.com>
2015-06-30 14:32:15 +0530

    Unit testing project added

---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Xml;
+using Newtonsoft.Json;
+using APLPX.Client;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //DataTable dt = new DataTable();
+             AplBusinessLayer objBL=new AplBusinessLayer();
+            APLPX.Client.localhost.StagingDbConfig stagingInfo =new APLPX.Client.localhost.StagingDbConfig();
+            //stagingInfo.
+            string result= objBL.TestConnection(stagingInfo);
+        }
+    }
+}
